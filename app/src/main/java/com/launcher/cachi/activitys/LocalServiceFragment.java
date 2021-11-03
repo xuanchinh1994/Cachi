3a7819cf84b4d5cbede154c1030eda0a297f9b2e
Chinh Bui <xuanchinh1994@gmail.com>
2021-11-03 07:17:57 +0700

    add button chrome

--- app/src/main/java/com/launcher/cachi/activitys/LocalServiceFragment.java
+++ app/src/main/java/com/launcher/cachi/activitys/LocalServiceFragment.java
@@ -1,15 +1,20 @@
 package com.launcher.cachi.activitys;
 
+import android.content.ComponentName;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.app.Activity;
+import android.widget.Toast;
+import android.service.dreams.DreamService;
+import android.widget.ImageView;
 
 
 import com.launcher.cachi.R;
@@ -52,8 +57,8 @@ public class LocalServiceFragment extends WoDouGameBaseFragment implements View.
 
         tv = (ImageButton) view.findViewById(R.id.local_tv);
         tour = (ImageButton) view.findViewById(R.id.local_tour);
-        ad1 = (ImageButton) view.findViewById(R.id.local_ad1);
-//        ad2 = (ImageButton) view.findViewById(R.id.local_ad2);
+//        ad1 = (ImageButton) view.findViewById(R.id.local_ad1);
+        ad2 = (ImageButton) view.findViewById(R.id.local_ad2);
 //        cate = (ImageButton) view.findViewById(R.id.local_cate);
 //        weather = (ImageButton) view.findViewById(R.id.local_weather);
 //        news = (ImageButton) view.findViewById(R.id.local_news);
@@ -62,8 +67,8 @@ public class LocalServiceFragment extends WoDouGameBaseFragment implements View.
 
         tv.setOnFocusChangeListener(mFocusChangeListener);
         tour.setOnFocusChangeListener(mFocusChangeListener);
-        ad1.setOnFocusChangeListener(mFocusChangeListener);
-//        ad2.setOnFocusChangeListener(mFocusChangeListener);
+//        ad1.setOnFocusChangeListener(mFocusChangeListener);
+        ad2.setOnFocusChangeListener(mFocusChangeListener);
 //        cate.setOnFocusChangeListener(mFocusChangeListener);
 //        weather.setOnFocusChangeListener(mFocusChangeListener);
 //        news.setOnFocusChangeListener(mFocusChangeListener);
@@ -71,24 +76,51 @@ public class LocalServiceFragment extends WoDouGameBaseFragment implements View.
 //        video.setOnFocusChangeListener(mFocusChangeListener);
 
         tv.setOnClickListener(this);
+        tour.setOnClickListener(this);
+        ad2.setOnClickListener(this);
 //        video.setOnClickListener(this);
 
         tv.setFocusable(true);
         tv.setFocusableInTouchMode(true);
         tv.requestFocus();
         tv.requestFocusFromTouch();
+
+        tour.setFocusable(true);
+        tour.setFocusableInTouchMode(true);
+        tour.requestFocus();
+        tour.requestFocusFromTouch();
+
+        ad2.setFocusable(true);
+        ad2.setFocusableInTouchMode(true);
+        ad2.requestFocus();
+        ad2.requestFocusFromTouch();
     }
 
     private void showImages() {}
 
     @Override
+    public void onAttachedToWindow () {
+        super.onAttachedToWindow();
+
+        setInteractive(false);
+        setFullscreen(true);
+
+        ImageView imageView = new ImageView(this);
+        imageView.setImageResource(R.drawable.dream);
+
+        setContentView(imageView);
+    }
+
+    @Override
     public void onClick(View v) {
+        Intent i;
         switch (v.getId()) {
             case R.id.local_tv:
-                Intent i;
+                Toast.makeText(context, "TV player pressed",
+                        Toast.LENGTH_SHORT).show();
                 PackageManager manager = getActivity().getPackageManager();
                 try {
-                    i = manager.getLaunchIntentForPackage("by.stari4ek.tvirl");
+                    i = manager.getLaunchIntentForPackage("ar.tvplayer.tv");
                     if (i == null)
                         throw new PackageManager.NameNotFoundException();
                     i.addCategory(Intent.CATEGORY_LAUNCHER);
@@ -97,9 +129,21 @@ public class LocalServiceFragment extends WoDouGameBaseFragment implements View.
 
                 }
                 break;
-            case R.id.local_ad1:
+            case R.id.local_ad2:
+                Toast.makeText(context, "Youtube player pressed",
+                        Toast.LENGTH_SHORT).show();
+                manager = getActivity().getPackageManager();
+                try {
+                    i = manager.getLaunchIntentForPackage("com.android.chrome");
+                    if (i == null)
+                        throw new PackageManager.NameNotFoundException();
+                    i.addCategory(Intent.CATEGORY_LAUNCHER);
+                    startActivity(i);
+                } catch (PackageManager.NameNotFoundException e) {
+
+                }
                 break;
-//            case R.id.local_ad2:
+//            case R.id.local_ad1:
 //                break;
 //            case R.id.local_weather:
 //                break;
@@ -110,17 +154,19 @@ public class LocalServiceFragment extends WoDouGameBaseFragment implements View.
 //            case R.id.local_news:
 //                break;
             case R.id.local_tour:
-                Intent i_youtube;
-                PackageManager manager_youtube = getActivity().getPackageManager();
+                Toast.makeText(context, "Youtube player pressed",
+                        Toast.LENGTH_SHORT).show();
+                manager = getActivity().getPackageManager();
                 try {
-                    i_youtube = manager_youtube.getLaunchIntentForPackage("com.google.android.youtube");
-                    if (i_youtube == null)
+                    i = manager.getLaunchIntentForPackage("com.google.android.youtube");
+                    if (i == null)
                         throw new PackageManager.NameNotFoundException();
-                    i_youtube.addCategory(Intent.CATEGORY_LAUNCHER);
-                    startActivity(i_youtube);
+                    i.addCategory(Intent.CATEGORY_LAUNCHER);
+                    startActivity(i);
                 } catch (PackageManager.NameNotFoundException e) {
 
                 }
+
                 break;
 //            case R.id.local_video:
 //                break;
